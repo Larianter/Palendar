bbd69adf6e2e61ad300532921f317e67565eea52
Larianter <lari.vainio@edu.turkuamk.fi>
2025-01-04 18:59:59 +0200

    Created models for db

--- Palendar/palendar_app/admin.py
+++ Palendar/palendar_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from palendar_app.models import User_Calendar, Event_Details, Users, Group_Calendar
 # Register your models here.
+admin.site.register(User_Calendar)
+admin.site.register(Event_Details)
+admin.site.register(Users)
+admin.site.register(Group_Calendar)
--- Palendar/palendar_app/models.py
+++ Palendar/palendar_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    #stores user account data
+    userID = models.IntegerField(primary_key=True)
+    account_name = models.CharField(max_length=24)
+    email = models.CharField()
+    password = models.CharField(max_length=12)
+    first_name = models.CharField
+    last_name = models.CharField
+
+class User_Calendar(models.Model):
+    #stores users own events
+    eventID = models.IntegerField(primary_key=True)
+    userID = models.ForeignKey(Users)
+
+class Event_Details(models.Model):
+    #data about user events
+    event_name = models.CharField(max_length=16)
+    event_desc = models.CharField(max_length=128)
+    event_date = models.DateTimeField(null=False)
+    eventID = models.ForeignKey(User_Calendar)
+    userID = models.ForeignKey(Users)
+
+class Group_Calendar(models.Model):
+    #stores all users' events in group
+    groupID = models.IntegerField(primary_key=True)
+    members = models.ForeignKey(Users)
+    eventID = models.ForeignKey(User_Calendar)
