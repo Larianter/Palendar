dda5f64cbc33577a8bf5644d998b3086c487c65d
Larianter <lari.vainio@edu.turkuamk.fi>
2025-01-11 13:32:21 +0200

    "This Week" now shows upcoming events correctly

--- Palendar/palendar_app/views.py
+++ Palendar/palendar_app/views.py
@@ -26,8 +26,8 @@ def inlogger(request):
             global current_user_id 
             current_user_id = Users.objects.get(account_name = current_user)
             user_events = EventDetails.objects.filter()
-            current_events = "\n".join([str(event) for event in user_events])
-            response = render(request, 'palendar_app/personal_calendar.html', {'account_name': current_user,'DEBUG_TEST': current_events})
+            #current_events = "\n".join([str(event) for event in user_events]) !! Deprecated !!
+            response = render(request, 'palendar_app/personal_calendar.html', {'account_name': current_user,'user_events': user_events})
         else:
             response = HttpResponse("credentials not found")
 
